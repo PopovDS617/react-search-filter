1712d3abcabf386a8ab002d899b02a42c2b9bd42
PopovDS617 <popovds.bm@gmail.com>
2022-08-08 21:57:50 +0300

    added table and search logic

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
 import './App.css';
-import {Users} from './data/users'
+import { Users } from './data/users';
 import { useState } from 'react';
 import Table from './Table';
 
 function App() {
-const [query,setQuery]=useState('')
+  const [query, setQuery] = useState('');
 
-const search=(data)=>{
-  return data.filter(item=>(
-    item.first_name.toLowerCase().includes(query)
-  ||item.last_name.toLowerCase().includes(query)
-  ||item.email.toLowerCase().includes(query)))
-}
- 
-console.log(query)
+  // более простое решение
+  //  const search = (data) => {
+  //     return data.filter(
+  //       (item) =>
+  //         item.first_name.toLowerCase().includes(query) ||
+  //         item.last_name.toLowerCase().includes(query) ||
+  //         item.email.toLowerCase().includes(query)
+  //     );
+  //   };
+
+  // более сложное решение. Если данных для фильтра будет много, то они заносятся в ключи
+  const keys = ['first_name', 'last_name', 'email'];
+  const search = (data) => {
+    return data.filter((item) =>
+      keys.some((key) => item[key].toLowerCase().includes(query))
+    );
+  };
+
+  console.log(query);
   return (
-    <div className='app' >
-      <input type='text' placeholder='Search...' className='search' onChange={e=>setQuery(e.target.value)}></input>
-  
-   
-    <Table data={search(Users)}  />
+    <div className="app">
+      <input
+        type="text"
+        placeholder="Поиск..."
+        className="search"
+        onChange={(e) => setQuery(e.target.value)}
+      ></input>
+
+      <Table searchResult={search(Users)} />
     </div>
   );
 }
--- src/Table.js
+++ src/Table.js
@@ -1,28 +1,33 @@
-import React from 'react'
+import React from 'react';
 
-const Table = ({data},props) => {
+const Table = ({ searchResult }) => {
   return (
-    <table>
-         
-        <tbody>
-            <tr>
-                <th>Name</th>
-                <th>Surname</th>
-                <th>E-mail</th>
+    <table className="table">
+      <tbody>
+        <tr>
+          <th>Name</th>
+          <th>Surname</th>
+          <th>E-mail</th>
+          <th>Gender</th>
+        </tr>
+
+        {searchResult.length ? (
+          searchResult.map((item) => (
+            <tr key={item.id}>
+              <td>{item.first_name}</td>
+              <td>{item.last_name}</td>
+              <td>{item.email}</td>
+              <td>{item.gender}</td>
             </tr>
-          
-            {data.map(item=>(
-                <tr key={item.id}>
-                <td>{item.first_name}</td>
-                <td>{item.last_name}</td>
-                <td>{item.email}</td>
-        </tr>))}
-
-            
-        
-        </tbody>
+          ))
+        ) : (
+          <tr>
+            <td className="not-found">nothing... </td>
+          </tr>
+        )}
+      </tbody>
     </table>
-  )
-}
+  );
+};
 
-export default Table
+export default Table;
