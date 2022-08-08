125a3f7220761182e719f847b88903e302c659a1
PopovDS617 <popovds.bm@gmail.com>
2022-08-08 20:21:38 +0300

    initial commit

---
+++ src/App.js
@@ -0,0 +1,27 @@
+import './App.css';
+import {Users} from './data/users'
+import { useState } from 'react';
+import Table from './Table';
+
+function App() {
+const [query,setQuery]=useState('')
+
+const search=(data)=>{
+  return data.filter(item=>(
+    item.first_name.toLowerCase().includes(query)
+  ||item.last_name.toLowerCase().includes(query)
+  ||item.email.toLowerCase().includes(query)))
+}
+ 
+console.log(query)
+  return (
+    <div className='app' >
+      <input type='text' placeholder='Search...' className='search' onChange={e=>setQuery(e.target.value)}></input>
+  
+   
+    <Table data={search(Users)}  />
+    </div>
+  );
+}
+
+export default App;
